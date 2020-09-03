a4838b0c2481ba9e977c55664e5fd3f1039a44da
Amir9eng <amirmukhtarme@gmail.com>
2020-09-03 18:17:34 +0100

    feat: hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
-      'my_blue': '#4F66D1',                 'my_gray': '#F7F5F7'
-    },
+      colors:{
+
+        'my_blue': '#4F66D1',                 'my_gray': '#F7F5F7'
+      }
+      },
   },
   variants: {},
   plugins: [],
